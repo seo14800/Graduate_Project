37c8afcebb667a558a7475fb00228660c5c40c99
VvV-PC\VvV <seo14800@gmail.com>
2017-06-01 17:12:01 +0900

    motor & moisture <-> pump

--- arduino/sketch_may20a/sketch_may20a.ino
+++ arduino/sketch_may20a/sketch_may20a.ino
@@ -1,24 +1,35 @@
-#include <Wire.h>
-#include <BH1750.h>
 
-BH1750 lightMeter;
+//#include <Wire.h>
+//#include <BH1750.h>
+
+//BH1750 lightMeter;
 int moisture = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  lightMeter.begin();
+  //lightMeter.begin();
   Serial.println("Running...");
+  pinMode(8, OUTPUT);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   moisture = analogRead(A0);
-  uint16_t lux = lightMeter.readLightLevel();
+
+  //if(moisture <= 300)
+  //  digitalWrite(8, HIGH);
+  //else
+  //  digitalWrite(8, LOW);
   
-  Serial.print("Light: ");
-  Serial.print(lux);
-  Serial.println(" lx");
+  
+  //uint16_t lux = lightMeter.readLightLevel();
+  
+  //Serial.print("Light: ");
+  //Serial.print(lux);
+  //Serial.println(" lx");
+
 
   Serial.print("moisture : ");
   Serial.println(moisture);
